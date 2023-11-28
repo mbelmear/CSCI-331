80c8ab45da623a6ff4e2eb76ebf54b5bf42c18fc
mbelmear <akmikey72@gmail.com>
2023-11-28 10:53:28 -0700

    Final Project

--- Final Project/scripts/apidata.js
+++ Final Project/scripts/apidata.js
@@ -18,10 +18,56 @@ function displayUser(user) {
     const userContainer = document.getElementById("apiData");
     userContainer.innerHTML = `
         <h2>${user.name.first} ${user.name.last}</h2>
-        <img src="${user.picture.large}" alt="User Image">
-        <p>Email: ${user.email}</p>
-        <p>Gender: ${user.gender}</p>
-        <p>Location: ${user.location.city}, ${user.location.country}</p>
+        <img src="${user.picture.large}" alt="A headshot of the random user!">
+        <br>
+        <details>
+            <p><b>Location:</b> ${user.location.city}, ${user.location.state}</p>
+            <p><b>Gender:</b> ${user.gender}</p>
+            <p><b>Email:</b> ${user.email}</p>
+        </details>
+        <br><hr><br>
+        <h3>Information About ${user.name.first}</h3>
+        <table>
+            <tr class="row">
+              <th>Title</th>
+              <th>Contact</th>
+              <th>Birthday</th>
+            </tr>
+            <tr>
+              <td>${user.name.title} ${user.name.first}</td>
+              <td>Phone: ${user.phone}</td>
+              <td>DOB: ${user.dob.date}</td> 
+            </tr>
+            <tr>
+              <td>${user.name.title} ${user.name.last}</td>
+              <td>Cell: ${user.cell}</td>
+              <td>Age: ${user.dob.age}</td> 
+            </tr>
+          </table>
+          <br><br>
+          <table>
+            <tr class="row">
+              <th>Street</th>
+              <th>Location</th>
+              <th>Coordinates</th>
+            </tr>
+            <tr>
+              <td>Name: ${user.location.street.name}</td>
+              <td>Country: ${user.location.country}</td>
+              <td>Latitude: ${user.location.coordinates.latitude} </td>
+            </tr>
+            <tr>
+              <td>Number: ${user.location.street.number}</td>
+              <td>Post Code: ${user.location.postcode}</td>
+              <td>Longitude: ${user.location.coordinates.longitude}</td>
+            </tr>
+          </table>
+        <h4>Sources</h4>
+        <ul>
+            <li><a href="https://www.w3.org/WAI/standards-guidelines/wcag/">WCAG</a></li>
+            <li><a href="https://blog.hubspot.com/website/web-accessibility">Web Accessibility</a></li>
+            <li><a href="https://medium.com/@ericapantojacs/semantic-html-cheat-sheet-9194768368bb">Semantic HTML</a></li>
+        </ul>
     `;
 }
 
